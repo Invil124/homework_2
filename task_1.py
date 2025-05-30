f1a5a51784260a94d9474b63a668d606c19e56f3
invil <invil124@gmail.com>
2025-05-31 02:38:11 +0300

    Commit

--- task_1.py
+++ task_1.py
@@ -1,3 +1,16 @@
-print('hellow')
 
 
+while True:
+    integer = input('Input integer:').strip()
+    if integer.isdigit():
+        integer = int(integer)
+        if integer % 3 == 0 and integer % 5 ==0:
+            print('ham')
+        elif integer % 3 == 0:
+            print('foo')
+        elif integer % 5 == 0:
+            print('bar')
+        else:
+            print('not dividing on 3 or 5 ')
+    else:
+        print('IT NOT AN INTEGER')
