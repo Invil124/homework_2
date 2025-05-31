bb88151c21550c89364be3933c8f67adecdaf717
invil <invil124@gmail.com>
2025-05-31 03:44:10 +0300

    Commit

--- task_1.py
+++ task_1.py
@@ -6,10 +6,13 @@ while True:
         integer = int(integer)
         if integer % 3 == 0 and integer % 5 ==0:
             print('ham')
+            break
         elif integer % 3 == 0:
             print('foo')
+            break
         elif integer % 5 == 0:
             print('bar')
+            break
         else:
             print('not dividing on 3 or 5 ')
     else:
---
+++ task_2.py
@@ -0,0 +1,40 @@
+
+## Вибачте що намудрив), провіряв можилвості map і трохи увлікся перевірками
+
+ITER = 0
+
+def check_integer(integer):
+    if not integer:
+        return False
+    elif not integer.isdigit():
+        global ITER
+        ITER += 1
+        print(f'{ITER} value is not an integer')
+        return False
+    else:
+        return int(integer)
+
+def input_two_integers():
+     list_integers = input("Enter two numbers: ").strip().split(" ")
+     if len(list_integers) != 2:
+         print('You should input two digits with space')
+         return [False,False]
+     else:
+         return list_integers
+
+while True:
+    a, b = map(check_integer, input_two_integers())
+    ITER = 0
+
+    if a is False or b is False:
+        continue
+    if a == b:
+        print(f'Numbers are equal {a}={b}')
+        break
+
+    if a > b:
+        print(f"{a} >{ b}")
+        break
+    else:
+        print(f"{a} < {b}")
+        break
